ce17836eed26117f7f434b5707dbd410f2609e95
sowmik-sec <sowmik.sec@gmail.com>
2023-03-18 18:12:54 +0600

    services api created. If I use environment variable then it is mandatory to write require(dotenv).config()

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+require("dotenv").config();
+
+// user: volunteerAdmin
+// pass: B7xOeWYGYM7rbGyi
 
 // middle wares
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.xgh8h2c.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  serverApi: ServerApiVersion.v1,
+});
+const run = async () => {
+  try {
+    const serviceCollection = client
+      .db("volunteerCoalition")
+      .collection("services");
+    app.get("/services", async (req, res) => {
+      const query = {};
+      const cursor = serviceCollection.find(query);
+      const services = await cursor.toArray();
+      res.send(services);
+    });
+  } finally {
+  }
+};
+
+run().catch((err) => console.error(err));
 
 app.get("/", (req, res) => {
   res.send("volunteer coalition server is running");
