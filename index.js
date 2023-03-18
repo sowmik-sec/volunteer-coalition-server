1eccb2cff5a49958c9d694cec3c31a6f79bf9dbc
sowmik-sec <sowmik.sec@gmail.com>
2023-03-18 18:16:37 +0600

    making system sequre. hiding environment variable

--- index.js
+++ index.js
@@ -5,9 +5,6 @@ const app = express();
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 
-// user: volunteerAdmin
-// pass: B7xOeWYGYM7rbGyi
-
 // middle wares
 app.use(cors());
 app.use(express.json());
